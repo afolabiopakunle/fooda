022438b1a25079cd3a4a09ce065e8a4c26c35705
Afolabi <opakunleafolabi@gmail.com>
2023-04-11 10:53:23 +0100

    recipe component created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { RecipeModel } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
+  providers: [RecipesService]
 })
 export class RecipeListComponent {
 
   recipe!: RecipeModel;
   @Output() clickedRecipe = new EventEmitter()
-  recipes: RecipeModel[] = [
-    new RecipeModel('Amala', 'Yoruba solid', 'https://i0.wp.com/kscuisine.com/wp-content/uploads/2014/04/IMG_1068.jpg?w=584&ssl=1'),
-    new RecipeModel('Fufu', 'Igbo solid', 'https://www.thespruceeats.com/thmb/Lkgiza_StWUtGA1UvDgf1EhBwic=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/yam-fufu-2138088-hero-01-3366d155060e480abaf33cc67031dc9d.jpg')
-  ];
+  recipes: RecipeModel[] = []
 
   showRecipe(recipe: RecipeModel) {
     this.recipe = recipe;
@@ -21,6 +20,10 @@ export class RecipeListComponent {
 
   onClickRecipe() {
     this.clickedRecipe.emit(this.recipe)
+  }
+
+  constructor(private recipeService: RecipesService) {
+    this.recipes = recipeService.getRecipes();
   }
 
 }
